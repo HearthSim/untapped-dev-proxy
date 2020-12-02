f8e5a46d43dea8717312024e94d1c724c01a8af7
Benedict Etzel <benedict@hearthsim.net>
2020-12-02 18:46:14 +0100

    Add code

---
+++ index.js
@@ -0,0 +1,37 @@
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const httpProxy = require("http-proxy");
+
+yargs(hideBin(process.argv)).command(
+	"proxy <sessionid>",
+	"start the server",
+	yargs => {
+		yargs.positional("sessionid", {
+			describe: "sessionid from an HSReplay.net cookie",
+			type: "string"
+		});
+	},
+	argv => {
+		proxy({ sessionId: argv.sessionid });
+	}
+).argv;
+
+function proxy(opts) {
+	const { sessionId } = opts;
+	const server = httpProxy
+		.createProxyServer({
+			target: {
+				protocol: "https:",
+				host: "hsreplay.net",
+				port: 443
+			},
+			changeOrigin: true,
+			headers: {
+				Cookie: `sessionid=${sessionId}` //,free-mode=1`
+			}
+		})
+		.listen(8080);
+	server.on("error", (e) => {
+		console.error(e);
+	});
+}
