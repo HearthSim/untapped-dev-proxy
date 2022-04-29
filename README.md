1ce4a935211352437f14bab19c0a8a5028420e75
Benedict Etzel <benedict@hearthsim.net>
2022-04-29 14:50:54 +0200

    Update for Untapped.gg services

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# hsreplaynet-dev-proxy
+# untapped-dev-proxy
 
 ## Install
 
 ```
-$ git clone git@github.com:HearthSim/hsreplaynet-dev-proxy.git
-$ cd hsreplaynet-dev-proxy
+$ git clone git@github.com:HearthSim/untapped-dev-proxy.git
+$ cd untapped-dev-proxy
 $ yarn install
 ```
 
 ## Usage
 
 ```
-$ yarn proxy abc123abc123abc123 -p 8080
+$ yarn proxy abc123abc123abc123 --service mtga -p 8080
 ```
--- index.js
+++ index.js
@@ -7,31 +7,42 @@ yargs(hideBin(process.argv)).command(
 	"start the server",
 	yargs => {
 		yargs.positional("sessionid", {
-			describe: "sessionid from an HSReplay.net cookie",
+			describe: "sessionid from an Untapped.gg cookie",
 			type: "string"
+		});
+		yargs.option("port", {
+			describe: "port to listen on",
+			type: "number",
+			default: 8080,
+		});
+		yargs.option("service", {
+			alias: "s",
+			describe: "Untapped.gg service to proxy",
+			choices: ["mtga", "accounts", "sbb"],
+			default: "mtga",
 		});
 	},
 	argv => {
-		proxy({ sessionId: argv.sessionid });
+		proxy({ sessionId: argv.sessionid, port: argv.port, service: argv.service });
 	}
 ).argv;
 
 function proxy(opts) {
-	const { sessionId } = opts;
+	const { sessionId, service, port } = opts;
 	const server = httpProxy
 		.createProxyServer({
 			target: {
 				protocol: "https:",
-				host: "hsreplay.net",
+				host: `api.${service}.untapped.gg`,
 				port: 443
 			},
 			changeOrigin: true,
 			headers: {
 				Cookie: `sessionid=${sessionId}`,
-				Referer: 'https://hsreplay.net/',
+				Referer: `https://${service}.untapped.gg/`,
 			}
 		})
-		.listen(8080);
+		.listen(port);
 	server.on("error", (e) => {
 		console.error(e);
 	});
